560bec7f645b7b6d08d3cfb229ea33529a5566be
isruu1 <israel_g@outlook.com>
2020-06-05 01:59:02 -0400

    first commit

---
+++ assignment1.py
@@ -0,0 +1,36 @@
+
+#question1
+# num1 = float(input("first number:"))
+# num2 = float(input("second number:"))
+#
+# print(num1 * num2 )
+
+#question2
+# print("My", "Name","is","James" , sep="**")
+
+#question3
+# name = input("what is your name:")
+# age  =  float(input("what is your age:"))
+# will_turn_100 = (2020 - age)
+# print("Hi {}, you were born {}".format(name,will_turn_100))
+
+# #question4
+# num1 = float(input("first number:"))
+# num2 = float(input("second number:"))
+# num3 = float(input("third number:"))
+# num4 = float(input("fourth number:"))
+# num5 = float(input("fifth number:"))
+#
+# print((num1 + num2 + num3 + num4 + num5)/5)
+
+#question 5
+# name = input("what is your name:")
+# time = float(input("Enter the time in 24 hour format:"))
+# if time>5 and time<12:
+#     print("Good morning",name)
+# elif time>12 and time < 18:
+#     print("Good afternoon",name)
+# elif time>18 and time<23:
+#     print("Good Evening",name)
+# else:
+#     print("Good Night",name)
